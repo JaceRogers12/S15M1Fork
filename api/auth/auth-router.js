86b2b601cf04751b82f89999bcce37ba626b9f77
JaceRogers12 <jacerogers12@gmail.com>
2024-05-16 13:58:50 -0600

    finished project

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -71,7 +71,7 @@ router.post("/login", checkUsernameExists, async (req, res, next) => {
 })
 
 router.get("/logout", async (req, res, next) => {
-  if (req.session) {
+  if (req.session.user) {
     req.session.destroy(err => {
       if (err) {
         next({status: 500, message: "There was an issue logging out"})
